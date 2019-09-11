8db3c0331387a1700c008bd04f089a175905e962
MrMencia <Avimencia@gmail.com>
2019-09-11 07:12:32 -0800

    Created new migration and confirmed functionality of database

--- WebAPISample/Migrations/201909111421273_ThirdMigration.Designer.cs
+++ WebAPISample/Migrations/201909111421273_ThirdMigration.Designer.cs
@@ -7,13 +7,13 @@ namespace WebAPISample.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.2.0-61023")]
-    public sealed partial class SecondMigration : IMigrationMetadata
+    public sealed partial class ThirdMigration : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(SecondMigration));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(ThirdMigration));
         
         string IMigrationMetadata.Id
         {
-            get { return "201909111214439_SecondMigration"; }
+            get { return "201909111421273_ThirdMigration"; }
         }
         
         string IMigrationMetadata.Source
